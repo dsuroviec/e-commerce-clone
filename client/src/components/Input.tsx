e4af4a4d00d50fd4f77c871f36818dbe157c0930
Darren S <dcsuroviec@gmail.com>
2021-09-28 12:40:09 -0400

    Working changes header and nav

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -5,7 +5,7 @@ export const Input = ({
 }: React.ComponentPropsWithoutRef<"input">) => {
     return (
         <input
-            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${className}`}
+            className={`shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${className}`}
             {...props}
         ></input>
     );
