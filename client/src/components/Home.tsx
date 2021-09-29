f0789bd0df3122175e378759cf9ca1cd23543ecb
Darren S <dcsuroviec@gmail.com>
2021-09-29 10:43:43 -0400

    Added popper arrow and overlay

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -9,30 +9,12 @@ import { HiSun } from "react-icons/hi";
 import { HiMoon } from "react-icons/hi";
 import { Menu } from "@headlessui/react";
 import { Popover, Transition } from "@headlessui/react";
-import { HiChevronDown } from "react-icons/hi";
+import { HiChevronDown, HiMail, HiChatAlt2 } from "react-icons/hi";
 import React, { Fragment } from "react";
 import { usePopper } from "react-popper";
+import { createPortal } from "react-dom";
 import ReactDOM from "react-dom";
-const solutions = [
-    {
-        name: "Insights",
-        description: "Measure actions your users take",
-        href: "##",
-        icon: IconOne,
-    },
-    {
-        name: "Automations",
-        description: "Create your own targeted content",
-        href: "##",
-        icon: IconTwo,
-    },
-    {
-        name: "Reports",
-        description: "Keep track of your growth",
-        href: "##",
-        icon: IconThree,
-    },
-];
+
 export const Home = () => {
     const { token, setToken } = useContext(TokenContext)!;
     const { user } = useContext(UserContext)!;
@@ -42,7 +24,16 @@ export const Home = () => {
     let [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
         null
     );
-    let { styles, attributes } = usePopper(referenceElement, popperElement);
+    const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(
+        null
+    );
+    let { styles, attributes } = usePopper(referenceElement, popperElement, {
+        // placement: "right",
+        modifiers: [
+            { name: "arrow", options: { element: arrowElement } },
+            { name: "offset", options: { offset: [-40, 8] } },
+        ],
+    });
 
     return (
         <>
@@ -75,52 +66,100 @@ export const Home = () => {
                                             aria-hidden="true"
                                         />
                                     </Popover.Button>
-
-                                    {ReactDOM.createPortal(
+                                    <Transition
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0"
+                                        enterTo="opacity-100"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <Popover.Overlay className="bg-black opacity-30 fixed inset-0" />
+                                    </Transition>
+                                    <Transition
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="opacity-0"
+                                        enterTo="opacity-100"
+                                        leave="transition ease-in duration-150"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <Popover.Overlay className="bg-black opacity-30 fixed inset-0" />
+                                    </Transition>
+                                    {createPortal(
                                         <Transition
                                             enter="transition ease-out duration-200"
-                                            enterFrom="opacity-0 translate-y-1"
-                                            enterTo="opacity-100 translate-y-0"
+                                            enterFrom="opacity-0"
+                                            enterTo="opacity-100"
                                             leave="transition ease-in duration-150"
-                                            leaveFrom="opacity-100 translate-y-0"
-                                            leaveTo="opacity-0 translate-y-1"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
                                         >
                                             <Popover.Panel
                                                 ref={setPopperElement}
                                                 style={styles.popper}
                                                 {...attributes.popper}
-                                                className="absolute z-10 w-96 max-w-sm px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl"
+                                                className=" w-80 bg-white "
+                                                data-popper-element
                                             >
-                                                <div className="overflow-hidden  shadow-lg ring-1 ring-black ring-opacity-5">
-                                                    <div className="relative  bg-white p-7">
-                                                        <span className="m-auto block ">
+                                                <div
+                                                    ref={setArrowElement}
+                                                    style={styles.arrow}
+                                                    data-arrow
+                                                />
+
+                                                <div className=" rounded-b-sm shadow-lg ring-1 ring-black ring-opacity-5">
+                                                    <div className="relative bg-white p-7">
+                                                        <div className="m-auto flex justify-center ">
                                                             Get help from our
                                                             experts 24/7
-                                                        </span>
-                                                        <span className="text-yellow-300 text-size-30 ">
-                                                            1 (800) pet-help
+                                                        </div>
+                                                        <span className="text-yellow-600 hover:underline font-medium text-3xl flex justify-center">
+                                                            1-800-672-4399
                                                         </span>
                                                     </div>
-                                                    <div className="p-4 bg-gray-50">
-                                                        <a
-                                                            href="##"
-                                                            className="flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                                                    <div className="flex bg-gray-50">
+                                                        <div className="flex-1 p-5 text-blue-500 border flex justify-center items-center">
+                                                            <HiChatAlt2
+                                                                size={30}
+                                                            />
+                                                            &nbsp;
+                                                            <p className="hover:underline ">
+                                                                Chat Live
+                                                            </p>
+                                                        </div>
+                                                        <div className="flex-1 text-blue-500  p-5 border flex justify-center items-center">
+                                                            <HiMail size={30} />
+                                                            &nbsp;
+                                                            <p className="hover:underline">
+                                                                Contact Us
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <div className=" text-blue-500 py-4 px-6 flex justify-between">
+                                                        <Link
+                                                            to="/"
+                                                            className="  hover:underline "
                                                         >
-                                                            <span className="flex items-center">
-                                                                <span className="text-sm font-medium text-gray-900">
-                                                                    Documentation
-                                                                </span>
-                                                            </span>
-                                                            <span className="block text-sm text-gray-500">
-                                                                Start
-                                                                integrating
-                                                                products and
-                                                                tools
-                                                            </span>
-                                                        </a>
+                                                            Track Order
+                                                        </Link>
+                                                        -
+                                                        <Link
+                                                            to="/"
+                                                            className=" hover:underline "
+                                                        >
+                                                            FAQs
+                                                        </Link>
+                                                        -
+                                                        <Link
+                                                            to="/"
+                                                            className=" hover:underline"
+                                                        >
+                                                            Shipping Info
+                                                        </Link>
                                                     </div>
                                                 </div>
-                                            </Popover.Panel>{" "}
+                                            </Popover.Panel>
                                         </Transition>,
                                         document.body
                                     )}
@@ -228,82 +267,3 @@ export const Home = () => {
         </>
     );
 };
-
-function IconOne() {
-    return (
-        <svg
-            width="48"
-            height="48"
-            viewBox="0 0 48 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-            <path
-                d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
-                stroke="#FB923C"
-                strokeWidth="2"
-            />
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
-                stroke="#FDBA74"
-                strokeWidth="2"
-            />
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
-                stroke="#FDBA74"
-                strokeWidth="2"
-            />
-        </svg>
-    );
-}
-
-function IconTwo() {
-    return (
-        <svg
-            width="48"
-            height="48"
-            viewBox="0 0 48 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-            <path
-                d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
-                stroke="#FB923C"
-                strokeWidth="2"
-            />
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
-                stroke="#FDBA74"
-                strokeWidth="2"
-            />
-        </svg>
-    );
-}
-
-function IconThree() {
-    return (
-        <svg
-            width="48"
-            height="48"
-            viewBox="0 0 48 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-            <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
-            <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
-            <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
-            <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
-            <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
-            <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
-        </svg>
-    );
-}
